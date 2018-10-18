86bd68d6ad84cd612887806fec0649a1586326d4
Brian-Lennon <blen0310@gmail.com>
2018-10-19 00:11:19 +0100

    Second commit
    Updating gitignore to ignore target folder, pom updated to add junit as a dependency.

---
+++ src/test/java/com/blen/csv/main/MainTest.java
@@ -0,0 +1,14 @@
+package com.blen.csv.main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class MainTest {
+
+    @Test
+    public void test(){
+        fail();
+    }
+
+}
